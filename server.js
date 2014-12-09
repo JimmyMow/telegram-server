65fcf8f94185b529d7760bc6455a79fdb029fbc6
Jack Mallers <jimmymowschess@gmail.com>
2014-12-09 11:20:12 -0600

    replaced dummy data with mongodb

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ var bodyParser = require('body-parser');
 var app = express();
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/telegram');
+var connection = mongoose.connection;
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  id:  String,
+  name: String,
+  email:   String,
+  picture: String,
+  password: String
+});
+
+var postSchema = new Schema({
+  id:  String,
+  body: String,
+  createdAt: { type: Date, default: Date.now },
+  user: String,
+  repost: String
+});
+
+var User = connection.model('User', userSchema);
+var Post = connection.model('Post', postSchema);
 
 // Passport
 var passport = require('passport')
@@ -22,34 +45,31 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-  for(var i = 0; i < users.length; i++) {
-    if(users[i].id === id) {
-      return done(null, users[i]);
-    }
-  }
-  done(null, null);
+  User.findOne({"id": id}, function(err, user) {
+    if(err){ return done(err); }
+
+    return done(null, user);
+  });
 });
 
 passport.use(new LocalStrategy({
     usernameField: 'id',
   },
   function(username, password, done) {
-    var user = null;
-    for(var i = 0; i < users.length; i++) {
-      if(users[i].id === username){
-        user = users[i]
+    User.findOne({"id": username}, function(err, user) {
+      if(err){ return done(err); }
+
+      if(!user) {
+        return done(null, false, { message: 'Incorrect username.' });
       }
-    }
 
-    if(!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
+      if(user.password !== password) {
 
-    if(user.password !== password) {
         return done(null, false, { message: 'Incorrect password.' });
-    }
+      }
 
-    return done(null, user);
+      return done(null, user);
+    });
   }
 ));
 
@@ -80,7 +100,11 @@ app.get('/api/users', function(req, res) {
       return res.send({ users: [] });
     }
   } else {
-    return res.send({ users: users });
+    User.find(function(err, users) {
+      if(err) { return res.send(err); }
+
+      return res.send({ users: users });
+    });
   }
 });
 
@@ -90,124 +114,72 @@ app.get('/api/logout', checkForAuthentication, function(req, res) {
 });
 
 app.get('/api/posts', function(req, res) {
-  var userPosts = []
   var username = req.query.user;
 
   if(username) {
-    for(var i = 0; i < posts.length; i++) {
-      if( (posts[i].user === username && posts[i].repost === null) || (posts[i].repost === username) ) {
-        userPosts.push(posts[i]);
-      }
-    }
-    res.send( {posts: userPosts} );
+    Post.find( { $or : [ { $and : [ { user : username }, { repost : null } ] }, { repost: username } ] }, function(err, posts) {
+      if(err) { return res.send(err); }
+
+      return res.send( {posts: posts} );
+    });
   } else {
-    res.send( {posts: posts} );
+    Post.find(function(err, posts) {
+      if(err) { return res.send(err); }
+
+      return res.send( {posts: posts} );
+    });
   }
 });
 
 app.get('/api/users/:id', function(req, res) {
-  var userID = req.params.id;
+  User.findOne({"id": req.params.id}, function(err, user) {
+    if(err) { return res.status(404).end(); }
 
-  for(var i = 0; i < users.length; i++) {
-    if(users[i].id === userID) {
-      return res.send( {user: users[i]} );
-    }
-  }
-  res.status(404).end();
+    return res.send({ user: user });
+  });
 });
 
 app.post('/api/users', function(req, res) {
-  var user = req.body.user;
-  users.push(user);
-  res.send({ user: user });
+  var user = new User({
+    id: req.body.user.id,
+    name: req.body.user.name,
+    email: req.body.user.email,
+    password: req.body.user.password
+  })
+  user.save(function(err, user){
+    if(err) { return res.status(500).end(); }
+    return res.send({ user: user });
+  });
 });
 
 app.post('/api/posts', checkForAuthentication, function(req, res) {
-  if(req.user.id === req.body.post.user) {
-    var post = req.body.post;
-    post.id = posts.length + 1;
-    posts.push(post);
+  if(req.user.id === req.body.post.user || req.user.id === req.body.post.repost) {
+    var post = new Post({
+      body: req.body.post.body,
+      user: req.body.post.user,
+      repost: req.body.post.repost,
+      createdAt: req.body.post.createdAt
+    });
 
-    return res.send({post: post});
+    post.save(function(err, post){
+      if(err) { return res.status(500).end(); }
+      return res.send({ post: post });
+    });
   } else {
     return res.status(403).end();
   }
 });
 
+app.delete('/api/posts/:id', function(req, res) {
+  var postID = req.params.id;
+
+  User.remove({id: postID}, function(err, result) {
+    if(err) { return res.send(err); }
+
+    return res.send({});
+  });
+});
+
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
-
-app.delete('/api/posts/:id', function(req, res) {
-  var postID = req.params.id;
-  var index = null;
-
-  for(var i = 0; i < posts.length; i++) {
-    if(posts[i].id == postID) {
-      index = posts.indexOf(posts[i]);
-      posts.splice(index, 1);
-      res.send({});
-    }
-  }
-
-  if(index === null) {
-    res.status(500).end();
-  }
-});
-
-
- var posts = [
-    {
-      id: 1,
-      body: 'Bobby Fischer is the greatest chess player of all time',
-      createdAt: new Date(),
-      user: 'JimmyMow',
-      repost: 'Fischer'
-    },
-    {
-      id: 2,
-      body: 'I am the greatest player of all time',
-      createdAt: new Date(),
-      user: 'Fischer',
-      repost: null
-    },
-    {
-      id: 3,
-      body: 'I\'m the champ #FuckBorris',
-      createdAt: new Date(),
-      user: 'Fischer',
-      repost: null
-    },
-    {
-      id: 4,
-      body: 'Let\'s hoop',
-      createdAt: new Date(),
-      user: 'JimmyMow',
-      repost: null
-    },
-    {
-      id: 5,
-      body: 'Bobby Fischer is the greatest chess player of all time',
-      createdAt: new Date(),
-      user: 'JimmyMow',
-      repost: null
-    }
-  ];
-
-  var users = [
-    {
-      id: 'JimmyMow',
-      name: 'Jack Mallers',
-      email: 'jimmymowschess@gmail.com',
-      picture: 'https://lh6.googleusercontent.com/-hBbaFeCzpFs/AAAAAAAAAAI/AAAAAAAAANA/r02VbznNRIs/w48-c-h48/photo.jpg',
-      password: '12345678'
-    },
-    {
-      id: 'Fischer',
-      name: 'Bobby Fischer',
-      email: 'bobbyfischer@gmail.com',
-      picture: 'https://pbs.twimg.com/profile_images/1264692865/bobby_fischer_01_normal.jpg',
-      password: '12345678'
-    }
-  ];
-
